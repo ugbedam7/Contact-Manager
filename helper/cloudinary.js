8ddacda737ce9df75265f44024981d44e38ac2f3
Damian Oguche <damianguch@yahoo.com>
2025-01-30 17:39:23 +0100

    Configured cloudinary for image upload

---
+++ helper/cloudinary.js
@@ -0,0 +1,26 @@
+const cloudinary = require('../config/cloudinary');
+
+/** uploadImage function expects a file path not the file's binary content.
+* 1. The cloudinary.uploader.upload() method requires the path to the file
+* as an argument. It does not directly accept file buffers or streams.
+
+2. To upload directly from binary content or streams, use Cloudinary's 
+  upload_stream method instead.
+*/
+const uploadImage = async (filePath) => {
+  try {
+    const result = await cloudinary.uploader.upload(filePath, {
+      folder: 'Contact-Images'
+    });
+
+    return {
+      url: result.secure_url,
+      publicId: result.public_id
+    };
+  } catch (error) {
+    console.log(error);
+    throw new Error(`${error.message}`);
+  }
+};
+
+module.exports = { uploadImage };
