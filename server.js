9b0c90f568269da6d9af7ba172ec17c04f718dd7
Damian Oguche <damianguch@yahoo.com>
2025-02-03 18:13:36 +0100

    Added cors option in server.js

--- server.js
+++ server.js
@@ -9,10 +9,29 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// CORS Options definition
+const corsOptions = {
+  origin: ['http://localhost:5173'],
+
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: [
+    'Authorization',
+    'Content-Type',
+    'X-Requested-With',
+    'Accept',
+    'Origin',
+    'Access-Control-Allow-Origin',
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Methods',
+    'Access-Control-Allow-Credentials'
+  ],
+  credentials: true
+};
+
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Routes
 app.use('/api/auth', authRoutes);
