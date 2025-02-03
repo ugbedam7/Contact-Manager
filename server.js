e5c54ae3966c5adfe029a84d66b2d1479fa050a3
Damian Oguche <damianguch@yahoo.com>
2025-02-03 20:08:17 +0100

    updated server.js with the cors origin

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS Options definition
 const corsOptions = {
-  origin: ['http://localhost:5173'],
+  origin: ['https://contact-app-fe.netlify.app'],
 
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: [
