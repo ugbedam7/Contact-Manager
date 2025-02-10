c0bf4532c6054922da52bdbd768a1aa81ef4b221
Damian Oguche <damianguch@yahoo.com>
2025-02-10 14:42:33 +0100

    Modified the request type between image update and contact update

--- routes/contact.js
+++ routes/contact.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const { Router } = express;
 const router = Router();
 const {
@@ -7,16 +7,23 @@ const {
   getContact,
   updateContact,
   deleteContact,
-  getQueryContacts
-} = require('../controllers/contact');
-const { isAuthenticated } = require('../middleware/authCheck');
-const { upload } = require('../middleware/upload');
+  getQueryContacts,
+  updateContactImage
+} = require("../controllers/contact");
+const { isAuthenticated } = require("../middleware/authCheck");
+const { upload } = require("../middleware/upload");
 
-router.post('/', upload.single('image'), isAuthenticated, createContact);
-router.get('/', getContacts);
-router.get('/', getQueryContacts);
-router.get('/:id', isAuthenticated, getContact);
-router.patch('/:id', upload.none(), isAuthenticated, updateContact);
-router.delete('/:id', isAuthenticated, deleteContact);
+router.post("/", upload.single("image"), isAuthenticated, createContact);
+router.get("/", getContacts);
+router.get("/", getQueryContacts);
+router.get("/:id", isAuthenticated, getContact);
+router.put("/:id", isAuthenticated, upload.none(), updateContact);
+router.patch(
+  "/:id",
+  isAuthenticated,
+  upload.single("image"),
+  updateContactImage
+);
+router.delete("/:id", isAuthenticated, deleteContact);
 
 module.exports = router;
